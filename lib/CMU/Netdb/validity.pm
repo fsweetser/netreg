4ec0606301bae4d85b61326bfb73bd5921c2ac64
Frank Sweetser <fs@wpi.edu>
2012-03-20 16:13:52 -0400

    Disable strict refs to allow symbolic reference to work

--- lib/CMU/Netdb/validity.pm
+++ lib/CMU/Netdb/validity.pm
@@ -1229,8 +1229,11 @@ sub verify_table_field {
   }
 
   my $TArray = "CMU::Netdb::structure::${table}_fields";
-  # Can't verify as the array doesn't exist.. but structure.pl tests this
-  return $in unless (defined @$TArray);
+  {
+    no strict 'refs';
+    # Can't verify as the array doesn't exist.. but structure.pl tests this
+    return $in unless (defined @{$TArray});
+  }
 
   my @SFields;
   eval '@SFields = @'.$TArray.';';
